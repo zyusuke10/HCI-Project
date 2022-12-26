6d4654934b32ac0167422cc75bc382957ac4da0d
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-12-26 16:31:06 +0000

    changes

--- src/pages/SpecificEventPage.js
+++ src/pages/SpecificEventPage.js
@@ -7,6 +7,7 @@ import priceTag from "../images/pricetag.svg";
 import whiteCloseIcon from "../images/white-close.svg";
 import { Link } from "react-router-dom";
 import { UseAppContext } from "../context/appContext";
+import closeIcon from "../images/close.svg";
 
 const SpecificEventPage = () => {
   const { events, eventId, addNewTicket, darkMode } = UseAppContext();
@@ -16,7 +17,7 @@ const SpecificEventPage = () => {
     <div className="specificEventPage-container">
       <Link to="/home">
         <img
-          src={whiteCloseIcon}
+          src={darkMode ? whiteCloseIcon : closeIcon}
           alt="close"
           className="specificEvent-close-icon"
         />
@@ -25,35 +26,57 @@ const SpecificEventPage = () => {
         <div className="specificEvent-image">
           <img src={image} alt="event" />
         </div>
-        <div className={darkMode ? "specificEvent-details-container-dark" : "specificEvent-details-container"}>
+        <div
+          className={
+            darkMode
+              ? "specificEvent-details-container-dark"
+              : "specificEvent-details-container"
+          }
+        >
           <div className="specificEvent-title">
-            <h4 className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{name}</h4>
+            <h4 className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+              {name}
+            </h4>
           </div>
           <div className="specificEvent-desc">
-            <h5 className={darkMode ? "dark-mode-text" : "light-mode-text2"}>Description</h5>
-            <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{desc}</p>
+            <h5 className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+              Description
+            </h5>
+            <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+              {desc}
+            </p>
           </div>
           <div className="specificEvent-details">
             <div className="specificEvent-location">
               <img src={pin} alt="pin" />
-              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{location}</p>
+              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+                {location}
+              </p>
             </div>
             <div className="specificEvent-phone">
               <img src={phone1} alt="phone" />
-              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{phone}</p>
+              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+                {phone}
+              </p>
             </div>
             <div className="specificEvent-time">
               <img src={clock} alt="clock" />
-              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{time}</p>
+              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+                {time}
+              </p>
             </div>
             <div className="specificEvent-price">
               <img src={priceTag} alt="price tag" />
-              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>{price}</p>
+              <p className={darkMode ? "dark-mode-text" : "light-mode-text2"}>
+                {price}
+              </p>
             </div>
 
             <Link to="/profile">
               <div
-                className={darkMode ? "addTicket-container-dark" : "addTicket-container"}
+                className={
+                  darkMode ? "addTicket-container-dark" : "addTicket-container"
+                }
                 onClick={() => addNewTicket(events[eventId])}
               >
                 Add Ticket
