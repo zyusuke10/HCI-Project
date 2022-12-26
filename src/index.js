f45717de07bfa82df1bd95394657115c8fd4cf38
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-12-26 15:15:24 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context/appContext";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </HashRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
