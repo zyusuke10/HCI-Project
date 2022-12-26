8e6c218d07ad511fa2c5f2ccd38981df416a9b12
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-12-26 15:42:33 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
   return (
     <BrowserRouter basename="/HCI-Project-Zport">
       <Routes>
-        <Route path="/HCI-Project-Zport" element={<SharedLayout />}>
-          <Route path="/home" element={<MainPage />} />
-          <Route path="/home/specific" element={<SpecificEventPage />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/payment" element={<Payment />} />
-          <Route path="/addEvent" element={<NewEvent />} />
+        <Route path="HCI-Project-Zport" element={<SharedLayout />}>
+          <Route path="home" element={<MainPage />} />
+          <Route path="home/specific" element={<SpecificEventPage />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/payment" element={<Payment />} />
+          <Route path="addEvent" element={<NewEvent />} />
         </Route>
         <Route index element={<Landing />} />
         <Route path="register" element={<Register />} />
